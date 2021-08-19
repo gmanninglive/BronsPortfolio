e1174324ab6eb67465b8303ec63ccab45761e459
gmanninglive <gmanning@live.co.uk>
2021-08-19 19:38:56 +0100

    .

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,14 +5,14 @@ const Hero = () => (
   <div className="container py-12 lg:pb-16">
     <div className="container w-full lg:w-3/4 justify-center">
       <Link to="/">
-      <img alt="Bronwen Latham" className="w-full" src={`${__dirname}header3.svg`} /></Link>
+      <img alt="Bronwen Latham" className="w-full" src="/header3.svg" /></Link>
     
     <h2>
       <span className="py-2 text-3xl font-roboto font-bold text-gray-900 md:flex md:justify-evenly">
       Journalist <br />
-      <img alt="point" className="hidden w-2 md:block" src={`${__dirname}point.svg`} />
+      <img alt="point" className="hidden w-2 md:block" src="point.svg" />
       Content Writer <br />
-      <img alt="point" className="hidden w-2 md:block" src={`${__dirname}point.svg`} />
+      <img alt="point" className="hidden w-2 md:block" src="point.svg" />
       Radio Presenter
       </span>
     </h2>
